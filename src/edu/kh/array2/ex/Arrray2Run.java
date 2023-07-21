4b9c1adcee599aaf364699a6f7354e1ad9743673
unknown <https://github.com/eee595398>
2023-07-21 16:45:45 +0900

    11

---
+++ src/edu/kh/array2/ex/Arrray2Run.java
@@ -0,0 +1,14 @@
+package edu.kh.array2.ex;
+
+public class Arrray2Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Array2Example ar = new Array2Example();
+		//ar.ex1();
+		//ar.ex2();
+		//ar.ex3();
+		ar.ex4();
+	}
+
+}
